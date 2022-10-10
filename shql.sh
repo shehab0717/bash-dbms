d9a6d77ccea7cf8d5f0234a114a9876fc128b72f
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-10-10 12:34:17 +0200

    first commit

---
+++ shql.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+PS="SHQL > "
+
+base="$HOME/bin/shql"
+PATH="$base:$base/connect:$base/create:$base/databases:$base/helpers:$PATH"
+
+while true
+do
+	read -p "shql > " cmd
+	cmd=". $cmd"
+	eval $cmd
+done
